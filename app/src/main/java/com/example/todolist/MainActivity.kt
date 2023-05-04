0fde81d5dec2cfd0a6bcfff147cfe752ceb0eb91
Milo4kaa <kukikun6666@gmail.com>
2023-05-04 13:50:10 +0300

    Первый экран v1.1

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.todolist.ui.theme.NoteActivity
 
 class MainActivity : AppCompatActivity() {
     public var i = ""
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onClickOpenNoteActivity (view : View){
+
+        val intent = Intent(this, NoteActivity::class.java)
+        startActivity(intent)
     }
 }
