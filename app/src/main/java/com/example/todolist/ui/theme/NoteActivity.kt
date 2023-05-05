87666f4009053ffa0a514c31514ea7db654b7293
Milo4kaa <kukikun6666@gmail.com>
2023-05-05 23:10:37 +0300

    Кнопки v1.0

--- app/src/main/java/com/example/todolist/ui/theme/NoteActivity.kt
+++ app/src/main/java/com/example/todolist/ui/theme/NoteActivity.kt
@@ -1,7 +1,10 @@
 package com.example.todolist.ui.theme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.todolist.MainActivity
 import com.example.todolist.R
 
 class NoteActivity : AppCompatActivity() {
@@ -9,4 +12,11 @@ class NoteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
     }
+    fun onClickBackToMain (view : View){
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun BackToMain(view: View) {}
 }
