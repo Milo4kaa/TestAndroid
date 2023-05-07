512c40c959c8439c0c6b3e4dd2d247752c1851d7
Milo4kaa <kukikun6666@gmail.com>
2023-05-07 12:38:31 +0300

    До биндинга

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.todolist.databinding.ActivityMainBinding
 import com.example.todolist.ui.theme.NoteActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
