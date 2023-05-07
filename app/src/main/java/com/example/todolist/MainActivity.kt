a4704c215e9f9cd9627ba19b79d713d063c525e2
Milo4kaa <kukikun6666@gmail.com>
2023-05-07 16:02:07 +0300

    Добавление записок v1.1

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -4,15 +4,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todolist.databinding.ActivityMainBinding
 import com.example.todolist.ui.theme.NoteActivity
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+
+     lateinit var  binding: ActivityMainBinding
+     private var adapter = NoteAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+    private fun init(){
+        binding.apply {
+            rcView.layoutManager = LinearLayoutManager(this@MainActivity)
+            rcView.adapter = adapter
+            NoteAdd.setOnClickListener {
+                val note = Note("","1")
+                adapter.addNote(note)
+            }
+        }
     }
     fun onClickOpenNoteActivity (view : View){
         val intent = Intent(this, NoteActivity::class.java)
---
+++ app/src/main/java/com/example/todolist/Note.kt
@@ -0,0 +1,3 @@
+package com.example.todolist
+
+data class Note(val title: String, val MainText: String)
